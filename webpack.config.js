901b50b34cf50a9698f34308621c3224f5057d47
sylph <124384687+sylph88@users.noreply.github.com>
2023-05-08 21:21:20 -0400

    init

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+}
